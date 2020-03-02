3ca9b4247d6859572f3ae3e58cd70ba31cfef2a2
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-03-02 11:25:09 -0600

    Ejemplo Suma

---
+++ Aritmeticos/ejemploSuma.asm
@@ -0,0 +1,24 @@
+.586
+.model flat
+.stack 1000h
+.data
+operando1 DQ 547893A512761723h
+operando2 DQ 4123A5B3F573418Bh
+.code
+inicio PROC
+	;	547893A512761723h
+	;					+
+	;	4123A5B3F573418Bh
+	;	Para sumarlos lo separamos
+	MOV EAX, DWORD PTR operando1 ; EAX = 12761723
+	MOV EBX, DWORD PTR operando2 ; EBX = F573418B
+	ADD EAX,EBX					 ; Resultado de la parte baja
+	;-----------------------------------------------------------
+	;Ahora vamos por la parte alta
+	MOV EBX, DWORD PTR operando1 + 4 ; EBX = 547893A5
+	MOV ECX, DWORD PTR operando2 + 4 ; ECX = 4123A5B3
+	;ADD EBX,ECX CON ESTE LE FALTA EL CARIE
+	ADC EBX,ECX ; iNCLUIMOS EL CARIE
+
+inicio ENDP 
+end

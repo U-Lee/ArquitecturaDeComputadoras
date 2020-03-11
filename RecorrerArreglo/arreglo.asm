f6a5e19edd0ae0f21241ca0d223533f1d5559469
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-03-11 11:13:47 -0600

    Arreglo 11/03/2020

---
+++ RecorrerArreglo/arreglo.asm
@@ -0,0 +1,22 @@
+.586
+.model flat
+.stack 1000h
+.data
+arreglo  DB 1,5,5,9,7,8,5,9,10
+contador DB 0
+.code
+inicio PROC
+	;Programa que nos dicie cuantas veces se repite un elemento en el arrglo (Palabra)
+    MOV EBX, OFFSET arreglo
+    MOV ECX,10
+    ciclo: 
+        MOV AL,[EBX] ; AL <- arreglo 
+        CMP AL,5 ; El número que queremos comparar
+        jz es_igual ; seguida de la instrucción JZ (salto si es cero)
+        INC EBX
+        continua: loop ciclo
+    es_igual:
+        INC contador
+        jmp continua
+inicio ENDP 
+end

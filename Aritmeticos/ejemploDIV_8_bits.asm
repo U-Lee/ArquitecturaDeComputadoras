114b9b7e0ddae6603cb812b2d2340c54f9693fbe
OswaldoHDZ <oswaldo.hernandez.juarezr@gmail.com>
2020-03-11 14:08:19 -0600

    Mejora

--- Aritmeticos/ejemploDIV_8_bits.asm
+++ Aritmeticos/ejemploDIV_8_bits.asm
@@ -3,15 +3,23 @@
 .stack 1000h
 .data
 valor DB 255
-digitos DB 3 DUP(0)
+residuos  DB 3 DUP(0)
+cocientes DB 3 DUP(0)
 .code
 inicio PROC
+	; AX se divide entre BL -> En este caso seria -> 255/10
+	; AH Contien el residuo
+	; AL Contiene el cociente con el signo
 	MOV AX,WORD PTR valor
 	MOV BL,10
 	DIV BL
-	MOV digitos,AH
+	MOV residuos,AH
+    MOV cocientes,AL
 	DIV BL
-	MOV digitos+1,AH
-	MOV digitos+2,AL
+	MOV residuos+1,AH
+    MOV cocientes+1,AL
+	DIV BL
+	MOV residuos+2,AH
+    MOV cocientes+2,AL
 inicio ENDP 
 end

b8bd63e4251a1c81318ab6504c70eda5c57383b0
U-Lee <32946965+U-Lee@users.noreply.github.com>
2020-03-11 16:59:31 -0600

    Ejemplo terminado | con visualización en arreglos

--- Aritmeticos/ejemploDIV_8_bits.asm
+++ Aritmeticos/ejemploDIV_8_bits.asm
@@ -2,24 +2,35 @@
 .model flat
 .stack 1000h
 .data
-valor DB 255
-residuos  DB 3 DUP(0)
-cocientes DB 3 DUP(0)
+valor DB 255 ; 8 BITS
+cociente DB 3 DUP(0)
+residuo DB 3 DUP(0)
+loquees DW 3 DUP(0) ;16 bits
+valores DB 3 DUP(0)
 .code
 inicio PROC
 	; AX se divide entre BL -> En este caso seria -> 255/10
 	; AH Contien el residuo
 	; AL Contiene el cociente con el signo
+
 	MOV AX,WORD PTR valor
-	MOV BL,10
+	MOV BL,10				
+	
+	DIV BL					
+
+	MOV cociente,AL
+	MOV residuo,AH
+	MOV loquees,AX
+	MOV valores,BL
+
+	MOV AH,0
 	DIV BL
-	MOV residuos,AH
-    MOV cocientes,AL
-	DIV BL
-	MOV residuos+1,AH
-    MOV cocientes+1,AL
-	DIV BL
-	MOV residuos+2,AH
-    MOV cocientes+2,AL
-inicio ENDP 
+
+	MOV cociente+1,AL
+	MOV residuo+1,AH
+	MOV loquees+2,AX
+	MOV valores+1,BL
+	
+
+	inicio ENDP
 end
